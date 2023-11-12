c88fdcacde8f0c523c318bbbe82f3c24e4fb9fe2
Mikhail Baydakov <m.baedakov@gmail.com>
2023-11-12 13:36:18 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+const popup = document.getElementById("popup");
+const main = document.getElementById("main");
+
+const popupAnimationClasses = [
+  { name: "popup_show", element: popup },
+  { name: "main_hide", element: main },
+];
+
+// if (localStorage.getItem("showPopup")) {
+//   animatePopup();
+// }
+
+const imageWheelButton = document.getElementById("imageWheelButton");
+const wheelArrow = document.getElementById("wheelArrow");
+const externalWheel = document.getElementById("externalWheel");
+const insideWheel = document.getElementById("insideWheel");
+
+const wheelAnimationClasses = [
+  { name: "welcome-offer-section__wheel-arrow_rotate", element: wheelArrow },
+  {
+    name: "welcome-offer-section__wheel-external_rotate",
+    element: externalWheel,
+  },
+  { name: "welcome-offer-section__wheel-inside_rotate", element: insideWheel },
+];
+
+function addAnimationClasses(animationArray) {
+  animationArray.forEach(({ name, element }) => element.classList.add(name));
+}
+
+function handleSpinButtonClick() {
+  localStorage.setItem("showPopup", true);
+
+  imageWheelButton.style.animation = "none";
+
+  addAnimationClasses(wheelAnimationClasses);
+}
+
+function animatePopup() {
+  addAnimationClasses(popupAnimationClasses);
+}
+
+imageWheelButton.addEventListener("click", handleSpinButtonClick);
+
+insideWheel.addEventListener("animationend", animatePopup);
